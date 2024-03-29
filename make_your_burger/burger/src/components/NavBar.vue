79baa5e5ef8e015ca5492d0b59172f73fe0867d1
LuisNunesH <luis.nunes@sptech.school>
2024-03-29 13:13:56 -0300

    Dev projeto

---
+++ make_your_burger/burger/src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+        <RouterLink to="/">
+            <img :alt="alt" :src="logo" id="logo" />
+        </RouterLink>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/pedidos">Pedidos</RouterLink>
+    </nav>
+</template>
+<script>
+    export default {
+        name: 'NavBar',
+        props: ["logo", "alt"]
+    }
+</script>
+<style scoped>
+    #nav {
+        background-color: 
+    }
+</style>
