25e8ad28d9bf9a8d535f07a359a18beee0d8136e
LuisNunesH <luis.nunes@sptech.school>
2024-05-28 16:20:38 -0300

    Dashboard pedidos

--- make_your_burger/burger/src/views/Pedidos.vue
+++ make_your_burger/burger/src/views/Pedidos.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="main-container">
     <h1>PEDIDOS</h1>
+    <Dashboard />
   </div>
 </template>
 <style>
 </style>
+<script>
+  import Dashboard from '@/components/Dashboard.vue';
+
+  export default {
+    name: 'Pedidos',
+    components: {
+      Dashboard
+    }
+  }
+</script>
